8b6f4a1a7c6681be1f7c9dd96ec774b434cf5a1e
arifrachcom <ardesain@Yahoo.com>
2018-04-30 11:53:38 +0700

    Config URI For RestAPI

---
+++ config/index.js
@@ -0,0 +1,23 @@
+const backendlessConfigurations = {
+    // Backendless server uri
+    BACKENDLESS_SERVER : 'https://api.backendless.com/954ED070-DAB6-29F9-FFC1-65B7C3AA0300/29456C43-95C4-0639-FFD1-D80284025400/data',
+
+    
+}
+  
+const trelloConfigurations = {
+    // Trello server uri
+    TRELLO_SERVER_URL: 'https://api.trello.com/1/',
+
+    API_KEY : '73c19e66cf4d2ccacc03417652778b56',
+
+    API_SECRET : '3967e6976f5e169a10cf0d586d2b850677746c100c14a6dbc122d36b30a1b233',
+
+    // MEMBER : 'https://api.trello.com/1/members',
+
+    CARDS : 'https://api.trello.com/1/boards/bD5pFONf/cards/?fields=name&member_fields=fullName&key=73c19e66cf4d2ccacc03417652778b56&token=c79ab242c9019b935787c2afdd8bd04e3b01d29df2a2462bb3ca8a69cf86b56a',
+
+    ORGANIZATIONS : 'https://api.trello.com/1/members/arifrach/organizations'
+};
+
+export { backendlessConfigurations, trelloConfigurations };
