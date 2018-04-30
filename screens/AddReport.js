9f0801378b0a1dcdf47ccdddc26c62b51d730ee6
arifrachcom <ardesain@Yahoo.com>
2018-04-30 12:09:37 +0700

    Merge Component Picker And Post Data to RestAPI

--- screens/AddReport.js
+++ screens/AddReport.js
@@ -14,6 +14,7 @@ import {
     Input,
     Textarea,
     Icon,
+    Picker,
     Footer,
     FooterTab
 } from 'native-base'
@@ -25,24 +26,150 @@ import {
 
 import CardTrello from '../components/CardTrello'
 
+import axios from 'axios'
+
+import {backendlessConfigurations, trelloConfigurations} from '../config/index';
+
+
 export default class AddReport extends Component{
+
+    state = {
+        // Initial Local State
+        title: '',
+        note: '',
+        isDone: false,
+        
+        cards: [],
+        reportCards: [],
+
+        cardOptions: [
+            {
+                id: 1,
+            }
+        ],
+        selected: undefined
+    }
+
+    allCards(){
+        // GET data All Cards from API Trello
+        axios.get(`${trelloConfigurations.CARDS}`).then((result)=>{
+            // set state cards to return result.data
+            this.setState({
+              cards: result.data
+            })
+        })
+    }
+    componentDidMount(){
+        // get method allCards() 
+        this.allCards()
+    }
+    onValueChangeYesterday(value: string) {
+        // set state selected value
+        this.setState({
+            reportCards: {
+                trelloCardId: value,
+                type: 'Yesterday',
+            }
+        });
+    }
+    onValueChangeToday(value: string) {
+        // set state selected value
+        this.setState({
+            reportCards: {
+                trelloCardId: value,
+                type: 'Today',
+            }
+        });
+    }
+    buttonInc(){
+        // declaration state
+        let cardOptions = this.state.cardOptions;
+        let counter = this.state.counter + 1
+
+        // push id to array of obj
+        cardOptions.push({
+            id: counter
+        })
+
+        // set state newPicker components
+        this.setState({
+            cardOptions,
+            counter
+        })
+    }
+
+    handleAddReport(){
+        // Initial constanta to get state value
+        const dataReportCard = {
+            ...this.state.data,
+            trelloCardId: this.state.reportCards.trelloCardId,
+            type: this.state.reportCards.type
+        }
+
+        // Initial constanta to get state value
+        const dataReports = {
+            ...this.state.data,
+            title: this.state.title,
+            isDone: this.state.isDone,
+            note: this.state.note
+        }
+
+        // Hardcode reportRelation post to reports tabel
+        const reportRelation = ['379D417E-3D88-DE60-FF15-854895614700'];
+        
+        // Axios post table reportCards from constanta value
+        axios
+            .post(`${backendlessConfigurations.BACKENDLESS_SERVER}/reportCards`, dataReportCard)
+            .then(result=>{
+                if(result.data){
+                    // if result.data == true
+                    axios
+                        // Axios post set relation one to one table report and report from objectId 
+                        .post(`${backendlessConfigurations.BACKENDLESS_SERVER}/reportCards/${result.data.objectId}/report:reports:1`, reportRelation)
+                        .then(result2=>{
+                            if(result2.data){
+                                axios
+                                // Axios post table reports from constanta value
+                                    .post(`${backendlessConfigurations.BACKENDLESS_SERVER}/reports`, dataReports)
+                                    .then(result3=>{
+                                        if(result3.data){
+                                            // Navigate to Report Detail
+                                            this.props.navigation.navigate('ReportDetail')
+                                        }
+                                    })
+                                }
+                            })
+                }
+            })
+
+        // axios
+        //     .post(`${backendlessConfigurations.BACKENDLESS_SERVER}/reports`, {
+        //         
+        //     })
+        //     .then(result=>{
+        //         //if success, get latest data from API
+        //         if(result.data){
+        //             this.props.navigation.navigate('Reports')
+        //         }
+        // })
+    }
     
     render(){
         return(
         <Container style={{backgroundColor: '#FFF'}}>
-            <Header>
+            <Header style={{backgroundColor: '#026aa7'}}>
                 <Left>
                     <Button transparent>
-                        <Icon name='arrow-back' />
+                        <Icon name='arrow-back' style={{color: '#fff'}}/>
                     </Button>
                 </Left>
                 <Body>
-                    <Title>Add Report</Title>
+                    <Title style={{color: '#fff'}}>Add Report</Title>
                 </Body>
                 <Right>
                     <Button transparent onPress={() =>
-                        this.props.navigation.navigate('ReportDetail')}>
-                        <Text>Save</Text>
+                        this.handleAddReport()}>
+                        <Text style={{color: '#fff'}}>Save</Text>
                     </Button>
                 </Right>
             </Header>
@@ -50,19 +177,78 @@ export default class AddReport extends Component{
                 <Form>
                     <View style={styles.itemForm}>
                         <Label>Title *</Label>
-                        <Input style={styles.inputForm}></Input>
+                        <Input 
+                            style={styles.inputForm}
+                            onChangeText={
+                                title=> this.setState({title})
+                            }
+                            value={this.state.title}
+                            placeholder="Title Report"
+                        />
                     </View>
                     <View style={styles.itemForm}>
                         <Label>What did you do yesterday ? *</Label>
-                        <CardTrello/>
+                        <View>
+                
+                            {this.state.cardOptions.map((cardOption)=> (
+                                <View key={cardOption.id}>
+                                    <Picker
+                                        iosHeader="Select Cards"
+                                        mode="dropdown"
+                                        placeholder="Card Trello API"
+                                        style={styles.pickerForm}
+                                        selectedValue={this.state.reportCards.trelloCardId}
+                                        onValueChange={this.onValueChangeYesterday.bind(this)}
+                                    >
+                                        {this.state.cards.map((card)=> (
+                                            <Picker.Item label={card.name} value={card.id} key={card.id}/>
+                                        ))} 
+                                    </Picker>
+                                </View>
+                            ))}
+                            
+                            <Button iconRight transparent style={styles.btnAdd} onPress={()=> this.buttonInc()}>
+                                <Text>Add Card</Text>
+                                <Icon name='add-circle' />
+                            </Button>
+                        </View>
                     </View>
                     <View style={styles.itemForm}>
                         <Label>What did you do today ? *</Label>
-                        <CardTrello/>
+                        <View>
+                
+                            {this.state.cardOptions.map((cardOption)=> (
+                                <View key={cardOption.id}>
+                                    <Picker
+                                        iosHeader="Select Cards"
+                                        mode="dropdown"
+                                        placeholder="Card Trello API"
+                                        style={styles.pickerForm}
+                                        selectedValue={this.state.reportCards.trelloCardId}
+                                        onValueChange={this.onValueChangeToday.bind(this)}
+                                    >
+                                        {this.state.cards.map((card)=> (
+                                            <Picker.Item label={card.name} value={card.id} key={card.id}/>
+                                        ))} 
+                                    </Picker>
+                                </View>
+                            ))}
+                            
+                            <Button iconRight transparent style={styles.btnAdd} onPress={()=> this.buttonInc()}>
+                                <Text>Add Card</Text>
+                                <Icon name='add-circle' />
+                            </Button>
+                        </View>
                     </View>
                     <View style={styles.itemForm}>
                         <Label>Note</Label>
-                        <Textarea rowSpan={5} bordered style={styles.textareaForm}/>
+                        <Textarea rowSpan={5} bordered style={styles.textareaForm} 
+                            onChangeText={
+                                note=> this.setState({note})
+                            }
+                            value={this.state.note}
+                            placeholder="Note Report"
+                        />
                     </View>
                 </Form>
             </Content>
@@ -108,6 +294,19 @@ const styles = StyleSheet.create({
     textareaForm: {
         borderColor: 'black',
         borderWidth: 1
+    },
+    pickerForm: {
+        flex: 1,
+        borderColor: 'black',
+        borderWidth: 1,
+        marginTop: 10,
+        marginRight: 20,
+        padding: 0,
+        height: 40,
+        alignSelf: 'stretch'
+    },
+    btnAdd: {
+        alignSelf: 'flex-end',
     },
     footerTabsColor:{
         backgroundColor: '#026aa7'
